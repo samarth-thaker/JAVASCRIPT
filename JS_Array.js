6bfd8f0c784ed026e99e8701043aa90b8ec9dcea
samarth-thaker <samarth.thaker07@gmail.com>
2023-12-27 00:46:16 +0530

    Arrays in JS

---
+++ JS_Array.js
@@ -0,0 +1,18 @@
+const arr = [0, 1, 2, 3, 4];
+console.log(arr[0]);
+const brr = ["shaktiman", "nagraj"];
+const arr2 = new Array(1,2,3,4);
+arr.push(6);
+console.log(arr);
+arr.pop();
+console.log(arr);
+arr.unshift(9);
+console.log(arr);
+arr.shift();
+console.log(arr);
+const arr3 = [5,6,7,8,9];
+//arr.push(arr3);
+//console.log(arr);
+//console.log(arr[5]);
+const num  = arr.concat(arr3);
+console.log(num);
